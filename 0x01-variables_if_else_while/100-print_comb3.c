6b48953f4f0235be2363636d60f946b3cf8eb0b5
vtakintola <abuhenry50@gmail.com>
2023-07-14 14:09:21 -0700

    Introduction to C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int p;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (p = 1; p < 10; p++)
+		{
+			if (n == p || n > p)
+				continue;
+			putchar(n % 10 + '0');
+			putchar(p % 10 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
