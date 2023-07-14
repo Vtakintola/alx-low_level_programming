d3ae887de4120b9c5251efd982cb32a4d1dbf8a5
vtakintola <abuhenry50@gmail.com>
2023-07-14 13:26:03 -0700

    Introduction to C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char p;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n % 10 + '0');
+	for (p = 'a'; p <= 'z'; p++)
+		putchar(p);
+	putchar('\n');
+	return (0);
+}
