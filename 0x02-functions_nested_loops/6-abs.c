6bdb666a91c2be50a23df7f0fc87676040288ee3
vtakintola <abuhenry50@gmail.com>
2023-07-18 08:44:41 -0700

    Introduction to C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - a function that checks for lowercase character.
+ *@n: parameter to be checked
+ *
+ * Return: Returns the absolute value of the parameter
+ */
+
+int _abs(int);
+{
+	if (n >= 0)
+		return (n);
+	else
+	{
+		n = (n * (-1));
+		return (n);
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 3. 3-islower.c: a function that checks for lowercase character.
 4. 4-isalpha.c:  a function that checks for alphabetic character.
 5. 5-sign.c: a function that prints the sign of a number.
+6. 6-abs.c: a function that computes the absolute value of an integer.
