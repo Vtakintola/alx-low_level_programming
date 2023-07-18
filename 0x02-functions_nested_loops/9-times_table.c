c7843b234045a5e0077767fc6772e625705420f7
vtakintola <abuhenry50@gmail.com>
2023-07-18 14:27:27 -0700

    Introduction to C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar('\n');
 			}
 			else
 			{
@@ -33,6 +34,7 @@ void times_table(void)
 					break;
 				_putchar(',');
 				_putchar(' ');
+				_putchar('\n');
 			}
 		}
 	}
