ec8367cf3a5073a95fadddc684de6bb33bdd9377
vtakintola <abuhenry50@gmail.com>
2023-07-19 06:30:37 -0700

    Introduction to C

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *int add - a function that adds up two numbers
+ *add - a function that adds up two numbers
  *@a: parameter to be added.
  *@b: parameter to be added.
- * Return: returns the sum of a and b.
+ *Return: returns the sum of a and b.
  */
 
 int add(int a, int b)
