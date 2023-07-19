80cb4ead839b71af6b13093abe979f709179da92
vtakintola <abuhenry50@gmail.com>
2023-07-19 11:48:05 -0700

    Introduction to C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -36,8 +36,17 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+			if (i >= 100)
+			{
+				_putchar(i / 100 + '0');
+				_putchar(((i / 10) % 10) + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
 			if (i == 98)
 				break;
 			_putchar(',');
