293d53f4bf4c6dfc6882107347259bd81dc733e3
vtakintola <abuhenry50@gmail.com>
2023-07-21 08:16:02 -0700

    Introduction to C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,37 @@
 #include "main.h"
 
 /**
- * print_times_table - a function that prints the 9 times table, starting with 0.
- *
+ * print_times_table - a function that .
+ *@n: parametr to check
  * Return: Always returns void.
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
+	int i, j, k;
 
-	for (i = 0; i <= n; i++)
+	if (n < 0 || n > 15)
 	{
-		for (j = n; j <= 9; j++)
-		{
-			k = i * j;
-			if (k < 10)
-			{
-				if (j == 0)
-				{
-					_putchar(k % 10 + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_Putchar(' ');
-					_putchar(k % 10 + '0');
-				}
-			}
-			else if (k < 100) 
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else if(k > 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(k / 100 + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar(k % 10 + '0');
-			}	
-		_putchar('\n');
+		return;
 	}
+	for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
+		{
+			if (k == 0)
+				_putchar(k % 10 + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (k < 100)
+					_putchar(' ');
+				if (k < 10)
+					_putchar(' ');
+				if (k > 99)
+					_putchar(k / 100 + '0');
+				if (k > 9)
+					_putchar(((k / 100) % 10) + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+	_putchar('\n');
 }
