44046204fcf1b2996846db085301416179da0c74
vtakintola <abuhenry50@gmail.com>
2023-07-21 08:44:16 -0700

    Introduction to C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,9 @@ void print_times_table(int n)
 	int k = i * j;
 
 	if (n < 0 || n > 15)
-	{
 		return;
-	}
 	for (i = 0; i <= n; i++)
+	{
 		for (j = 0; j <= n; j++)
 		{
 			if (k == 0)
@@ -36,5 +35,6 @@ void print_times_table(int n)
 			}
 		}
 		_putchar('\n');
+	}
 	_putchar('\n');
 }
