e65c49c8b0b3ced58b65186b9acb4e0b8dedad1d
vtakintola <abuhenry50@gmail.com>
2023-07-24 15:40:28 -0700

    Introduction to C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("fizz");
@@ -21,8 +21,9 @@ int main(void)
 			printf("fizzbuzz");
 		else
 			printf("%d", i);
-		if (i != 99)
+		if (i != 100)
 			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
