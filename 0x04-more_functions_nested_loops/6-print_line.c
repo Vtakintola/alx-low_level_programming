024e05941c747e2679808d4ed5e2dbef2ce8803d
vtakintola <abuhenry50@gmail.com>
2023-07-24 12:14:00 -0700

    Introduction to C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - a function that prints a atraight line
+ * @n: the lenght of the line
+ * Return: returns void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (i > 0)
+		_putchar('_');
+	_putchar('\n');
+}
