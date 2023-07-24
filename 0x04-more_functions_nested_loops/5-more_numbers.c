8c146fa0489cf27bdbb40a5373db1bdb443f8073
vtakintola <abuhenry50@gmail.com>
2023-07-24 11:53:47 -0700

    Introduction to C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,8 +16,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-				_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
