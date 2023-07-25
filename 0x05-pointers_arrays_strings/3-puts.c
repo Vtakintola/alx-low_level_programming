85f7aa18adf4c3564756d9bdc80a9c642a8b5470
vtakintola <abuhenry50@gmail.com>
2023-07-25 13:45:31 -0700

    Introduction to C

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string.
+ * @str: sting to be printed.
+ * Return: reurns void.
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
