770e8cd02d8687903b096c2ff74c5f7c496d97b0
vtakintola <abuhenry50@gmail.com>
2023-07-26 11:59:29 -0700

    Introduction to C

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concanates two strings.
+ * @dest: destination string
+ * @src: source string
+ * @n: byte to copy
+ * Return: returns dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= n; j++)
+	{
+		if (s[j] == '\0')
+			break;
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
